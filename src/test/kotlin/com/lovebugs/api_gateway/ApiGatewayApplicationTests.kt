50919cfe0764c2a15ef62f6e0dfad78a117efb33
rivercity310 <h970126@gmail.com>
2024-07-15 11:24:58 +0900

    Initialize Project

---
+++ src/test/kotlin/com/lovebugs/api_gateway/ApiGatewayApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.lovebugs.api_gateway
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ApiGatewayApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}

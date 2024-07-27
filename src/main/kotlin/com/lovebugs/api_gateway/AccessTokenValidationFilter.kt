561802bbd6216f08ba10e033186ef122e602f8ac
rivercity310 <h970126@gmail.com>
2024-07-27 11:46:23 +0900

    feat: 토큰 검증 필터 업데이트

--- src/main/kotlin/com/lovebugs/api_gateway/AccessTokenValidationFilter.kt
+++ src/main/kotlin/com/lovebugs/api_gateway/AccessTokenValidationFilter.kt
@@ -1,4 +1,4 @@
-package com.lovebugs.api_gateway.filter
+package com.lovebugs.api_gateway
 
 import org.slf4j.LoggerFactory
 import org.springframework.cloud.gateway.filter.GatewayFilter
@@ -22,6 +22,7 @@ class AccessTokenValidationFilter(
         private const val AUTHORIZATION_HEADER = HttpHeaders.AUTHORIZATION
         private const val AUTHORIZATION_PREFIX = "Bearer"
         private val log = LoggerFactory.getLogger(AccessTokenValidationFilter::class.java)
+        private val NO_CHECK_URL = arrayOf("/token/v1/validation")
     }
 
     class Config
@@ -29,25 +30,32 @@ class AccessTokenValidationFilter(
     override fun apply(config: Config): GatewayFilter {
         return GatewayFilter { exchange: ServerWebExchange, chain: GatewayFilterChain ->
             val request: ServerHttpRequest = exchange.request
+            val path = request.path.toString()
             val authHeader = request.headers.getFirst(AUTHORIZATION_HEADER)
 
-            // Request Header에 token이 존재하지 않을 때
-            if (authHeader == null || !authHeader.startsWith(AUTHORIZATION_PREFIX)) {
-                return@GatewayFilter handleBadRequest(exchange)
-            }
+            val check = NO_CHECK_URL.none { it == path }
 
-            val token = authHeader.replace(AUTHORIZATION_PREFIX, "").trim()
+            if (check) {
+                log.info("Path: {}, Check: {}, authHeader: {}", path, check, authHeader)
 
-            webClient.post()
-                .uri("/token/v1/validation")
-                .headers { it.set(AUTHORIZATION_HEADER, "$AUTHORIZATION_PREFIX $token") }
-                .retrieve()
-                .bodyToMono(Void::class.java)
-                .then(chain.filter(exchange)) // 토큰이 유효할 때 필터 체인 계속 진행
-                .onErrorResume {
-                    log.error("Error: ${it.message}")
-                    handleBadRequest(exchange)
+                // Request Header에 token이 존재하지 않을 때
+                if (authHeader == null || !authHeader.startsWith(AUTHORIZATION_PREFIX)) {
+                    return@GatewayFilter handleBadRequest(exchange)
                 }
+
+                val token = authHeader.replace(AUTHORIZATION_PREFIX, "").trim()
+
+                webClient.get()
+                    .uri("/token/v1/validation")
+                    .headers { it.set(AUTHORIZATION_HEADER, "$AUTHORIZATION_PREFIX $token") }
+                    .retrieve()
+                    .bodyToMono(Void::class.java)
+                    .then(chain.filter(exchange)) // 토큰이 유효할 때 필터 체인 계속 진행
+                    .onErrorResume {
+                        log.error("Error: ${it.message}")
+                        handleBadRequest(exchange)
+                    }
+            }
 
             // 필터링 제외 경로는 필터 체인 계속 진행
             chain.filter(exchange)
